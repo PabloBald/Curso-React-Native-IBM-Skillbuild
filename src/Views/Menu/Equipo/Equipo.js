022b4cfe02854195a0783e9aabf649220ed66157
PabloBald <pablo.baldor91@gmail.com>
2021-10-30 15:18:03 -0300

    Agregada y linkeada la fuente Poppins, Creadas las cards en Equipo

--- src/Views/Menu/Equipo/Equipo.js
+++ src/Views/Menu/Equipo/Equipo.js
@@ -1,11 +1,92 @@
 import React from "react";
-import { View, Text} from "react-native";
+import { View, Text,Image,StyleSheet} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 const Equipo = ()=>{
     return(
-        <View>
-            <Text>Equipo..</Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <View style={styles.card} >
+                <View style={styles.card__top}>
+                    <View><Image style={styles.image} source={require("../../../assets/imgs/Menu/foto.png")} resizeMode="contain"/></View>
+                    
+                    <Text style={styles.title}>CARLOS BIANCUSCO</Text>
+                </View>
+                <View style={styles.card__bottom}>
+                    <Text style={styles.description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia cumque alias dolores nemo aperiam!</Text>
+                </View>
+            </View>
+            <View style={styles.card} >
+                <View style={styles.card__top}>
+                    <View><Image style={styles.image} source={require("../../../assets/imgs/Menu/foto.png")} resizeMode="contain"/></View>
+                    
+                    <Text style={styles.title}>CARLOS BIANCUSCO</Text>
+                </View>
+                <View style={styles.card__bottom}>
+                    <Text style={styles.description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia cumque alias dolores nemo aperiam!</Text>
+                </View>
+            </View>
+            <View style={styles.card} >
+                <View style={styles.card__top}>
+                    <View><Image style={styles.image} source={require("../../../assets/imgs/Menu/foto.png")} resizeMode="contain"/></View>
+                    
+                    <Text style={styles.title}>CARLOS BIANCUSCO</Text>
+                </View>
+                <View style={styles.card__bottom}>
+                    <Text style={styles.description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia cumque alias dolores nemo aperiam!</Text>
+                </View>
+            </View>
+            <View style={styles.card} >
+                <View style={styles.card__top}>
+                    <View><Image style={styles.image} source={require("../../../assets/imgs/Menu/foto.png")} resizeMode="contain"/></View>
+                    
+                    <Text style={styles.title}>CARLOS BIANCUSCO</Text>
+                </View>
+                <View style={styles.card__bottom}>
+                    <Text style={styles.description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia cumque alias dolores nemo aperiam!</Text>
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 export default Equipo;
+
+const styles = StyleSheet.create({
+container:{
+    backgroundColor:"#FFF",
+    padding:20,
+    fontFamily: "Poppins"
+},
+card:{
+    backgroundColor:"#C4C4C4",
+    padding:20,
+    borderRadius:10,
+    flexWrap: "wrap",
+    maxWidth: 400,
+    marginVertical:10
+    
+},
+card__top:{
+    flexDirection:"row",
+    alignItems:"center",
+},
+image:{
+    borderRadius:10
+},
+card__bottom:{
+marginVertical:10
+},
+title:{
+    fontFamily:"Poppins",
+    fontSize:18,
+    fontWeight:"bold",
+    color:"#858585",
+    marginLeft:10,
+    
+},
+description:{
+    fontFamily:"Poppins",
+    fontSize:16,
+    color:"#858585"
+},
+
+})
--- src/Views/Menu/SobreApp/SobreApp.js
+++ src/Views/Menu/SobreApp/SobreApp.js
@@ -12,9 +12,9 @@ const SobreApp = ()=>{
 
                 <Text style={styles.titulo}>Instrucciones</Text>
 
-                <Text style={styles.texto}><Text style={styles.subtitulo}>Home: </Text>Esta aplicación busca resolver el principal problema de nuestro cliente Paula cuya necesidad era poder tener a la vista el clima de su locación actual y el de otras que pueda llegar a necesitar</Text>
+                <Text style={styles.texto}><Text style={styles.subtitulo}>Home: </Text>Cuando se abre la aplicacion muestra el clima de la ubicación actual.</Text>
 
-                <Text style={styles.texto}><Text style={styles.subtitulo}>Search: </Text>Se utiliza para realizar busquedas de zonas deseadas. Al hacer click sobre una te redirige hacia la vista correspondiente para mostrar el clima de esa locacion.</Text>
+                <Text style={styles.texto}><Text style={styles.subtitulo}>Search: </Text>Se utiliza para realizar busquedas de zonas deseadas. Al hacer click sobre una te redirige hacia la vista correspondiente para mostrar el clima de esa locación.</Text>
 
                 <Text style={styles.texto}><Text style={styles.subtitulo}>Favorites: </Text>Muestra las locaciones guardadas donde podras acceder al clima de la misma o borrarla haciendo swipe hacia la izquierda.</Text>
                 
@@ -37,27 +37,31 @@ const styles = StyleSheet.create({
         marginHorizontal:40,
     },
     titulo:{
+        fontFamily:"Poppins",
         color:"#858585",
         fontSize: 24,
         fontWeight:"bold",
-        marginTop:30
+        marginTop:30,
+        
     },
     subtitulo:{
+        fontFamily:"Poppins",
         color:"#858585",
         fontWeight:"bold",
         marginTop: 30,
+        
         
     },
     texto:{
         color: "#858585",
         fontSize:16,
-        letterSpacing:0.3,
-        marginTop:30
+        marginTop:30,
+        fontFamily:"Poppins"
     },
     ultimoTexto:{
         color: "#858585",
+        fontFamily:"Poppins",
         fontSize:16,
-        letterSpacing:0.3,
         marginTop:30,
         marginBottom:30
     }
