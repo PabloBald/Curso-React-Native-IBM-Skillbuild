c03bb18fcf457ad12a1353f757df635ac30abc46
RobertoEzequielPaez <roberto.ezequiel.paez@gmail.com>
2021-11-23 04:02:19 -0300

    Se modifican tamaños estáticos por dinámicos. Falta terminar de arreglar el contenido de la tarjeta de clima actual

--- src/screens/Home/Main/index.js
+++ src/screens/Home/Main/index.js
@@ -77,9 +77,9 @@ export default Main = ({navigation}) => {
         <>
           <View style={styles.mainContainer}>
             <View style={styles.top}>
-              <View>
-                <View style={{fontSize: 14}}>
-                  <Text>Current Location</Text>
+              <View style={styles.top__current}>
+                <View style={{fontSize: 12}}>
+                  <Text>Ubicación actual</Text>
                 </View>
                 <View>
                   <Text style={{fontWeight: 'bold', fontSize: 24}}>
@@ -93,76 +93,89 @@ export default Main = ({navigation}) => {
                   size={24}
                   onPress={handleSaved}
                 />
-                {saved ? <Text>Saved!</Text> : null}
+                {saved ? <Text>Guardado!</Text> : null}
               </View>
             </View>
             <View style={styles.weatherIcon}>
               <Image
+                style={styles.weatherIcon__icon}
                 source={{
                   uri: `https://openweathermap.org/img/wn/${weather[0].weather[0].icon}@2x.png`,
                 }}
                 resizeMode="contain"
               />
             </View>
+            {/* //TODO: Terminar de acomodar contenido de la tarjeta. */}
             <View style={styles.card}>
-              <View style={styles.card__top}>
-                <View style={styles.card__top_currentTemp}>
-                  <Text style={{color: '#858585', fontSize: 12}}>
-                    Current temperature
-                  </Text>
-                  <Text
-                    style={{
-                      fontSize: 72,
-                      fontWeight: 'bold',
-                      color: '#858585',
-                    }}>
-                    {weather[0].main.temp}º
-                  </Text>
-                </View>
-                <View style={styles.card__top_maxMin}>
-                  <View>
+              <View style={styles.card__content}>
+                <View style={styles.card__top}>
+                  <View style={styles.card__top_currentTemp}>
+                    <Text style={{color: '#858585', fontSize: 12}}>
+                      Current temperature
+                    </Text>
+                    <Text
+                      style={{
+                        fontSize: 72,
+                        fontWeight: 'bold',
+                        color: '#858585',
+                      }}>
+                      {weather[0].main.temp}º
+                    </Text>
+                  </View>
+                  <View style={styles.card__top_maxMin}>
                     <View>
-                      <Text style={styles.card__title}>Max</Text>
+                      <View>
+                        <Text style={styles.card__title}>Max</Text>
+                      </View>
+                      <View>
+                        <Text
+                          style={{
+                            color: '#858585',
+                            fontSize: 14,
+                            fontWeight: 'bold',
+                          }}>
+                          {weather[0].main.temp_max}
+                        </Text>
+                      </View>
                     </View>
                     <View>
-                      <Text
-                        style={{
-                          color: '#858585',
-                          fontSize: 14,
-                          fontWeight: 'bold',
-                        }}>
-                        {weather[0].main.temp_max}
-                      </Text>
+                      <View>
+                        <Text style={{color: '#858585', fontSize: 12}}>
+                          Min
+                        </Text>
+                      </View>
+                      <View>
+                        <Text
+                          style={{
+                            color: '#858585',
+                            fontSize: 14,
+                            fontWeight: 'bold',
+                          }}>
+                          {weather[0].main.temp_min}
+                        </Text>
+                      </View>
+                      {/* //FIXME: Buscar la rua correcta para generar las lista */}
+                      <FlatList
+                        data={weather[1].daily[0].humidity}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.id}
+                        horizontal
+                      />
                     </View>
                   </View>
+                </View>
+                <View style={styles.card__bottom}>
                   <View>
-                    <View>
-                      <Text style={{color: '#858585', fontSize: 12}}>Min</Text>
-                    </View>
-                    <View>
-                      <Text
-                        style={{
-                          color: '#858585',
-                          fontSize: 14,
-                          fontWeight: 'bold',
-                        }}>
-                        {weather[0].main.temp_min}
-                      </Text>
-                    </View>
+                    <Text style={styles.card__title}>Today October 20</Text>
                   </View>
-                </View>
-              </View>
-              <View style={styles.card__bottom}>
-                <View>
-                  <Text style={styles.card__title}>Today October 20</Text>
-                </View>
-                <View>
-                  <FlatList
-                    data={weather[0].perHour}
-                    renderItem={renderItem}
-                    keyExtractor={item => item.id}
-                    horizontal
-                  />
+                  <View>
+                    <FlatList
+                      data={weather[0].perHour}
+                      renderItem={renderItem}
+                      keyExtractor={item => item.id}
+                      horizontal
+                    />
+                  </View>
                 </View>
               </View>
             </View>
--- src/screens/Home/Main/styles.js
+++ src/screens/Home/Main/styles.js
@@ -1,57 +1,79 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
   },
+
   top: {
-    flex: 1,
+    alignSelf: 'stretch',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginHorizontal: 40,
-    marginTop: 20,
+    marginTop: '7.10%',
   },
+
+  top__current: {
+    marginLeft: '10.25%',
+  },
+
   top__fav_icon: {
-    width: 50,
     alignItems: 'center',
+    marginRight: '3%',
+    marginTop: '2%',
+    width: 70,
   },
+
   weatherIcon: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: '4.73%',
+    maxHeight: '18.72%',
+    overflow: 'hidden',
   },
+
+  weatherIcon__icon: {
+    width: '100%',
+    height: '100%',
+  },
+
   card: {
-    flex: 3,
-    backgroundColor: '#C4C4C4',
-    width: 310,
-    height: 330,
-    borderRadius: 16,
-    marginHorizontal: 40,
-    marginVertical: 50,
-    paddingHorizontal: 15,
+    flex: 1,
+    justifyContent: 'center',
   },
+
+  card__content: {
+    backgroundColor: '#C4C4C4',
+    width: '79.48%',
+    height: '80.48%',
+    marginLeft: '10.25%',
+    borderRadius: 16,
+  },
+
   card__title: {
     color: '#858585',
+    marginTop:'6.06%',
     fontSize: 12,
   },
+
   card__top: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginVertical: 15,
+    // marginVertical: 15,
   },
 
   renderItem: {
-    marginRight: 20,
-    marginVertical: 10,
+    // marginRight: 20,
+    // marginVertical: 10,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'yellow',
   },
   renderItem__hour: {
     color: '#858585',
   },
   renderItem__icon: {
-    marginVertical: 5,
+    // marginVertical: 5,
   },
   renderItem__temp: {
     color: '#858585',
