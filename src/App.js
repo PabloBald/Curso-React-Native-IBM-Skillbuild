29783d109dc7fef5e471117726cd6e128776585c
RobertoEzequielPaez <roberto.ezequiel.paez@gmail.com>
2021-11-23 04:06:32 -0300

    Se elimina el componente CustomHeader porque no se utilizaba.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-
 import NavigationBar from './components/NavigationBar';
-import CustomHeader from './components/CustomHeader';
 // import { About, Favorites, Home, Login, Menu, NextForecast, Search, SignUp, Team, Utilities} from './screens';
 
 const App = () => {
   return (
     <NavigationContainer>
       <NavigationBar /> 
-      <CustomHeader />
     </NavigationContainer>
   );
 };
