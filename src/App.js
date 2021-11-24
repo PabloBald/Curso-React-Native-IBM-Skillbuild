0026b7c1b820f8407f5f29e6f8a9aa73842dcbd3
RobertoEzequielPaez <roberto.ezequiel.paez@gmail.com>
2021-11-24 20:13:54 -0300

    Bloqueo los mensajes de error que apararecen en la aplicacion. Siguen funcionando los que aparecen por consola

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import NavigationBar from './components/NavigationBar';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { LogBox } from 'react-native';
 // import { About, Favorites, Home, Login, Menu, NextForecast, Search, SignUp, Team, Utilities} from './screens';
 
 const App = () => {
+  LogBox.ignoreAllLogs();
+
   return ( 
     <SafeAreaProvider>
       <NavigationContainer>
