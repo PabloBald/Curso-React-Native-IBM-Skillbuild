e24c66bbfd5ace2d1b33f601aca280396deea244
RobertoEzequielPaez <roberto.ezequiel.paez@gmail.com>
2021-11-24 18:23:29 -0300

    Se agrega el área segura para que respete las dimensiones del dispositivo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import NavigationBar from './components/NavigationBar';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 // import { About, Favorites, Home, Login, Menu, NextForecast, Search, SignUp, Team, Utilities} from './screens';
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <NavigationBar /> 
-    </NavigationContainer>
+  return ( 
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <NavigationBar />
+      </NavigationContainer>
+    </SafeAreaProvider>
+
   );
 };
 
