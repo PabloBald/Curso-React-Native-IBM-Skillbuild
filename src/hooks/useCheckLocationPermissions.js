3426a1d3c20f967196b33449247cc8381a7e309f
RobertoEzequielPaez <roberto.ezequiel.paez@gmail.com>
2021-11-25 04:33:41 -0300

    Quito líneas innecesarias

--- src/hooks/useCheckLocationPermissions.js
+++ src/hooks/useCheckLocationPermissions.js
@@ -21,13 +21,11 @@ const useCheckLocationPermissions = () => {
       if (Platform.OS === 'ios') {
         status = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
         setPermissionStatus(status);
-        // return status;
       }
 
       if (Platform.OS === 'android') {
         status = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
         setPermissionStatus(status);
-        // return status;
       }
     } catch (error) {
       console.error(error);
