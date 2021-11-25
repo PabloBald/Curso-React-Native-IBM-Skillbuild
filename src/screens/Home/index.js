e959cb49b2a14e3564df6315985c45e9b5ecdc4c
RobertoEzequielPaez <roberto.ezequiel.paez@gmail.com>
2021-11-25 04:58:30 -0300

    Acomodo el título

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <Stack.Navigator initialRouteName="Main">
       <Stack.Screen name="Main"         component={Main}         options={{headerShown: false, }} />
-      <Stack.Screen name="NextForecast" component={NextForecast} options={{gestureEnabled:true}} />
+      <Stack.Screen name="NextForecast" component={NextForecast} options={{headerTitle:'Pronóstico extendido'}} />
     </Stack.Navigator>
   );
 };
