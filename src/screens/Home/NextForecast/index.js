2ba4f9453f8ca898aec765409f56b0ef1c857de2
RobertoEzequielPaez <roberto.ezequiel.paez@gmail.com>
2021-11-25 05:11:24 -0300

    Reestructurado y funcionando
    Se muestran los días, temperatura máxima y mínima, y los íconos del estado del clima de la ciudad que aparece en Home

--- src/screens/Home/NextForecast/index.js
+++ src/screens/Home/NextForecast/index.js
@@ -1,79 +1,56 @@
 import styles from './styles';
 import React, {useState, useEffect} from 'react';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {View, Text, Image} from 'react-native';
+import useWeatherIcon from '../../../hooks/useWeatherIcon';
 
-const NextForecastCard = ({day}) => {
+const NextForecastCard = weekday => {
   const [cardDay, setCardDay] = useState('');
+  const [weatherIcon, setWeatherIcon] = useState({});
 
   useEffect(() => {
-    const date = new Date();
-    const todayDayNumber = date.getDay();
-    day = parseInt(day);
     let nameOfDay = '';
-
-    let dayNumber = todayDayNumber + day;
-    dayNumber = dayNumber > 6 ? (dayNumber -= 7) : dayNumber;
-
-    switch (dayNumber) {
-      case 0:
-        nameOfDay = 'Domingo';
-        break;
-      case 1:
-        nameOfDay = 'Lunes';
-        break;
-      case 2:
-        nameOfDay = 'Martes';
-        break;
-      case 3:
-        nameOfDay = 'Miércoles';
-        break;
-      case 4:
-        nameOfDay = 'Jueves';
-        break;
-      case 5:
-        nameOfDay = 'Viernes';
-        break;
-      case 6:
-        nameOfDay = 'Sábado';
-        break;
+    switch (weekday.day) {
+      case 'Sun': nameOfDay = 'Domingo'; break;
+      case 'Mon': nameOfDay = 'Lunes'; break;
+      case 'Tue': nameOfDay = 'Martes'; break;
+      case 'Wed': nameOfDay = 'Miércoles'; break;
+      case 'Thu': nameOfDay = 'Jueves'; break;
+      case 'Fri': nameOfDay = 'Viernes'; break;
+      case 'Sat': nameOfDay = 'Sábado'; break;
     }
-    // console.log(nameOfDay);
     setCardDay(nameOfDay);
+  }, []);
+
+  useEffect(() => {
+    setWeatherIcon(useWeatherIcon(weekday.daily.weather[0].icon))
   }, []);
 
   return (
     <View style={styles.itemContainer}>
       <Text style={styles.dayText}>{cardDay}</Text>
       <Image
-        source={require('../../../assets/images/weatherIcons/WeatherIcon.png')}
+        source={weatherIcon.src}
         resizeMode="contain"
-        style={styles.weatherIcon}
-      />
-      <Text style={styles.tempText}>30.0°</Text>
-      <Text style={styles.tempText}>10.0°</Text>
+        style={styles.weatherIcon} />
+      <Text style={styles.tempText}>Max: {weekday.daily.temp.max.toFixed(1)}°</Text>
+      <Text style={styles.tempText}>Min: {weekday.daily.temp.min.toFixed(1)}°</Text>
     </View>
   );
 };
 
 const NextForecast = ({route}) => {
-  const {weather} = route.params;
-
-  console.log('weather nf', route);
+  const weather = route.params.weather[1];
 
   return (
     <View style={styles.mainContainer}>
-      <View style={styles.textContainer}>
-        <Text style={styles.text}>Pronóstico extendido</Text>
-      </View>
       <View style={styles.itemListContainer}>
-        <NextForecastCard day="1" key="1" />
-        <NextForecastCard day="2" key="2" />
-        <NextForecastCard day="3" key="3" />
-        <NextForecastCard day="4" key="4" />
-        <NextForecastCard day="5" key="5" />
-        <NextForecastCard day="6" key="6" />
-        <NextForecastCard day="7" key="7" />
+        <NextForecastCard day={String(new Date(weather.daily[1].dt * 1000)).slice(0, 3)} key="1" daily={weather.daily[1]}/>
+        <NextForecastCard day={String(new Date(weather.daily[2].dt * 1000)).slice(0, 3)} key="2" daily={weather.daily[2]}/>
+        <NextForecastCard day={String(new Date(weather.daily[3].dt * 1000)).slice(0, 3)} key="3" daily={weather.daily[3]}/>
+        <NextForecastCard day={String(new Date(weather.daily[4].dt * 1000)).slice(0, 3)} key="4" daily={weather.daily[4]}/>
+        <NextForecastCard day={String(new Date(weather.daily[5].dt * 1000)).slice(0, 3)} key="5" daily={weather.daily[5]}/>
+        <NextForecastCard day={String(new Date(weather.daily[6].dt * 1000)).slice(0, 3)} key="6" daily={weather.daily[6]}/>
+        <NextForecastCard day={String(new Date(weather.daily[7].dt * 1000)).slice(0, 3)} key="7" daily={weather.daily[7]}/>
       </View>
     </View>
   );
