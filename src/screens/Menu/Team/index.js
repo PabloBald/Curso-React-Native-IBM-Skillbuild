add294feb3f0349edef20d1d2c8914a8adb61f51
PabloBald <pablo.baldor91@gmail.com>
2021-11-27 17:16:11 -0300

    Agregadas descripciones de equipo. Cambios en la card.

--- src/screens/Menu/Team/index.js
+++ src/screens/Menu/Team/index.js
@@ -11,17 +11,21 @@ export default Team = () => {
           <View>
             <Image
               style={styles.image}
-              source={require('../../../assets/images/avatar.png')}
+              source={{uri:'https://i.imgur.com/2pcDtrx.png',height:100,width:100}}
               resizeMode="contain"
             />
           </View>
-
-          <Text style={styles.title}>CARLOS BIANCUSCO</Text>
+          <View style={{marginLeft:'5%'}}>
+            <Text style={styles.title}>Roberto Paez 
+          </Text>
+          <View>
+          <Text style={styles.subtitle}>Scrum Master / Developer</Text>
+          </View>
+          </View>
         </View>
         <View style={styles.card__bottom}>
           <Text style={styles.description}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
-            cumque alias dolores nemo aperiam!
+          Roberto se encargo de coordinar al equipo y ayudar a lidiar con las dificultades que iban surgiendo, ademas de la parte del desarrollo realizo tareas de manejo y orden en el repositorio de GitHub.
           </Text>
         </View>
       </View>
@@ -30,17 +34,21 @@ export default Team = () => {
           <View>
             <Image
               style={styles.image}
-              source={require('../../../assets/images/avatar.png')}
+              source={{uri:'https://i.imgur.com/9n2gC0z.jpg',height:100,width:100}}
               resizeMode="contain"
             />
           </View>
-
-          <Text style={styles.title}>CARLOS BIANCUSCO</Text>
+          <View style={{marginLeft:'5%'}}>
+            <Text style={styles.title}>Pablo Baldor 
+          </Text>
+          <View>
+          <Text style={styles.subtitle}>Product Owner / Developer</Text>
+          </View>
+          </View>
         </View>
         <View style={styles.card__bottom}>
           <Text style={styles.description}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
-            cumque alias dolores nemo aperiam!
+        Durante el desarrollo del proyecto, ademas del aporte como desarrollador, Pablo fue el encargado de captar los requerimientos del cliente y establecer las tareas para el equipo. Tambien realizó aportes como desarrollador en la estructura del proyecto y las interfaces de usuario.
           </Text>
         </View>
       </View>
@@ -49,17 +57,21 @@ export default Team = () => {
           <View>
             <Image
               style={styles.image}
-              source={require('../../../assets/images/avatar.png')}
+              source={{uri:'https://i.imgur.com/YQAnhi0.png',height:100,width:100}}
               resizeMode="contain"
             />
           </View>
-
-          <Text style={styles.title}>CARLOS BIANCUSCO</Text>
+          <View style={{marginLeft:'5%'}}>
+            <Text style={styles.title}>Leandro Muzzupappa 
+          </Text>
+          <View>
+          <Text style={styles.subtitle}>UX Lead / Developer</Text>
+          </View>
+          </View>
         </View>
         <View style={styles.card__bottom}>
           <Text style={styles.description}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
-            cumque alias dolores nemo aperiam!
+            A lo largo del ciclo de desarrollo del proyecto, Leandro fue el encargado de realizar el diseño e implementaciónes de las interfaces de usuario y prototipado en herramientas UX como Figma.
           </Text>
         </View>
       </View>
@@ -68,17 +80,44 @@ export default Team = () => {
           <View>
             <Image
               style={styles.image}
-              source={require('../../../assets/images/avatar.png')}
+              source={{uri:'https://i.imgur.com/6pPXImu.png',height:100,width:100}}
               resizeMode="contain"
             />
           </View>
-
-          <Text style={styles.title}>CARLOS BIANCUSCO</Text>
+          <View style={{marginLeft:'5%'}}>
+            <Text style={styles.title}>Leandro Cappiello 
+          </Text>
+          <View>
+          <Text style={styles.subtitle}>UI/UX Developer</Text>
+          </View>
+          </View>
         </View>
         <View style={styles.card__bottom}>
           <Text style={styles.description}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
-            cumque alias dolores nemo aperiam!
+          Leandro fue el encargado de realizar labores de diseño y desarrollo de interfaces de usuario. También estuvo a cargo del prototipado físico del proyecto aportando, las bases para el diseño de la aplicación.
+          </Text>
+        </View>
+      </View>
+      <View style={styles.card}>
+        <View style={styles.card__top}>
+          <View>
+            <Image
+              style={styles.image}
+              source={{uri:'https://i.imgur.com/KhJikum.png',height:100,width:100}}
+              resizeMode="contain"
+            />
+          </View>
+          <View style={{marginLeft:'5%'}}>
+            <Text style={styles.title}>Denisse Lemos 
+          </Text>
+          <View>
+          <Text style={styles.subtitle}>CI/CD Developer</Text>
+          </View>
+          </View>
+        </View>
+        <View style={styles.card__bottom}>
+          <Text style={styles.description}>
+          Por su experiencia previa como desarrolladora en React Native, Denisse fue la encargada de facilitar las tareas del equipo aportando rápidas soluciones a problemas conocidos, ayudando a todos los miembros del equipo a tener una primer experiencia de desarrollo mucho más amigable. También fue la encargada del deploy de la aplicacón.
           </Text>
         </View>
       </View>
--- src/screens/Menu/Team/styles.js
+++ src/screens/Menu/Team/styles.js
@@ -13,6 +13,7 @@ const styles= StyleSheet.create({
     borderRadius: 10,
     maxWidth: 400,
     marginBottom: 30,
+    elevation: 3
   },
   card__top: {
     flexDirection: 'row',
@@ -25,11 +26,19 @@ const styles= StyleSheet.create({
     marginVertical: 10,
   },
   title: {
+    letterSpacing:1.5,
+    flexDirection:'column',
     fontFamily: 'Poppins',
     fontSize: 18,
     fontWeight: 'bold',
     color: '#858585',
-    marginLeft: 10,
+    
+  },
+  subtitle:{
+    fontFamily: 'Poppins',
+    fontSize: 15,
+    color: '#858585',
+    fontStyle:'italic'
   },
   description: {
     fontFamily: 'Poppins',
