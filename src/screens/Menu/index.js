7204869118f8fe19f10576d75767d6b16e291e54
RobertoEzequielPaez <roberto.ezequiel.paez@gmail.com>
2021-11-28 21:53:41 -0300

    cambios mínimo

--- src/screens/Menu/index.js
+++ src/screens/Menu/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {About, Login, SignUp, Team, Utilities} from '../index';
+import {About, Team, Utilities} from '../index';
 import Main from  './Main';
 
 const Stack = createNativeStackNavigator();
@@ -9,15 +9,12 @@ export default Menu = () => {
   return (
     /**
      * Stack correspondiente al item Menu
-     * Contiene las screens Main, About, Login, SignUp, Team, Utilities
+     * Contiene las screens Main, About, Team, Utilities
      */
     <Stack.Navigator initialRouteName="Main">
       <Stack.Screen name="Main"      component={Main}  options={{ headerShown: false }}/>
-
       <Stack.Screen name="Team"      component={Team}      options={{ title: 'El equipo'     }}/>
       <Stack.Screen name="About"     component={About}     options={{ title: 'Sobre la App'  }}/>
-      <Stack.Screen name="Login"     component={Login}     options={{ title: 'Ingresar'      }}/>
-      <Stack.Screen name="SignUp"    component={SignUp}    options={{ title: 'Registrarse'   }}/>
       <Stack.Screen name="Utilities" component={Utilities} options={{ title: 'Links útiles'  }}/>
     </Stack.Navigator>
   );
